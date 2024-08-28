4b2ce54723183c21d5719a32fdb5789d4ce1ae97
Adrian Uriel <adrianurielvergaraa@gmail.com>
2024-08-27 17:34:15 -0700

    Navlink and ODS

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,26 +1,85 @@
+import { Link, NavLink } from "react-router-dom";
+
 export const Nav = () => {
   return (
     <>
       <div className=" flex items-center">
         <div className="bg-white h-20 w-full flex items-center pl-10 border-b-2 border-zinc-300">
-          <img className="h-14 w-14 pb-2" src="/icono.svg" alt="icon" />
-          <p className="font-bold pl-2 text-2xl">Ecopolis</p>
+          <Link to="/" className="flex justify-center items-center">
+            <img className="h-28 w-28 mt-2" src="/Icono.png" alt="icon" />
+          </Link>
           <div className="ml-28 flex justify-evenly ">
-            <p className="text-base font-semibold  mx-5 text-black">Inicio</p>
-            <p className="text-base font-semibold  mx-5 text-black">ODS</p>
-            <p className="text-base font-semibold mx-5 text-black">
-              Informacion del Equipo
-            </p>
-            <p className="text-base font-semibold mx-5 text-black">Quimica</p>
-            <p className="text-base font-semibold mx-5 text-black">
-              Ciencias Politicas
-            </p>
-            <p className="text-base font-semibold mx-5 text-black">
-              Arte y Cultura
-            </p>
-            <p className="text-base font-semibold mx-5 text-black">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-green-600 text-base font-semibold  mx-5"
+                  : "text-base font-semibold  mx-5 text-black hover:text-green-600"
+              }
+            >
+              Inicio
+            </NavLink>
+            <NavLink
+              to="/ods"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-green-600 text-base font-semibold  mx-5"
+                  : "text-base font-semibold  mx-5 text-black hover:text-green-600"
+              }
+            >
+              ODS
+            </NavLink>
+            <NavLink
+              to="/info"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-green-600 text-base font-semibold  mx-5"
+                  : "text-base font-semibold  mx-5 text-black hover:text-green-600"
+              }
+            >
+              Informacion Del Equipo
+            </NavLink>
+            <NavLink
+              to="/quimica"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-green-600 text-base font-semibold  mx-5"
+                  : "text-base font-semibold  mx-5 text-black hover:text-green-600"
+              }
+            >
+              Quimica
+            </NavLink>
+
+            <NavLink
+              to="/matematicas"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-green-600 text-base font-semibold  mx-5"
+                  : "text-base font-semibold  mx-5 text-black hover:text-green-600"
+              }
+            >
               Matematicas
-            </p>
+            </NavLink>
+            <NavLink
+              to="/ciencias"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-green-600 text-base font-semibold  mx-5"
+                  : "text-base font-semibold  mx-5 text-black hover:text-green-600"
+              }
+            >
+              Ciencias Politicas
+            </NavLink>
+            <NavLink
+              to="/arte"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-green-600 text-base font-semibold  mx-5"
+                  : "text-base font-semibold  mx-5 text-black hover:text-green-600"
+              }
+            >
+              Arte Y Cultura
+            </NavLink>
           </div>
         </div>
       </div>
--- src/components/ods.tsx
+++ src/components/ods.tsx
@@ -1,7 +1,10 @@
 export const Ods = () => {
   return (
-    <div>
-      <div></div>
+    <div className="flex items-center justify-center mx-20 my-10 flex-col">
+      <div className="font-bold text-2xl my-10">
+        Objetivos de Desarrollo Sostenible{" "}
+      </div>
+      <img src="/ods.png" alt="ods" className="h-[500px] w-[500px]" />
     </div>
   );
 };
